0624ec25c879a33d01e683426c6d1ae5a0c36d3e
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-02 21:04:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(speakers)
- speakers.each_with_index do |name, speaker| 
+ speakers.each_with_index do |name, index| 
  "Hello, #{name}! You'll be assigned to room #{index}!"
  end
 end
